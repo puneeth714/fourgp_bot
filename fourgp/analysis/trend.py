e3d7235dbf4ce16e2ca7394d4088dc091025707d
Puneeth <kpuneeth714@gmail.com>
2022-01-26 19:52:48 +0530

    None

--- fourgp/analysis/trend.py
+++ fourgp/analysis/trend.py
@@ -182,23 +182,113 @@ class Trend:
     def __ticker_price__(self) -> float:
         return self.data[self.config["primary_timeframe"]].tail(1)["Close"].values[0]
 
-    def __get_candles__(self, number_of) -> pd.DataFrame:
-        return self.data[self.config["primary_timeframe"]].tail(number_of)
+    def __get_candles__(self, timeframe,count=-1) -> pd.DataFrame:
+        """Get last "count" number of candles for each timeframe
+
+        Args:
+            count (int, optional): number of candles for each timeframe. Defaults to -1 (all candles).
+
+        Returns:
+            pd.DataFrame: candles (pd.DataFrame)
+        """
+        __data__ = self.data if count == -1 \
+                    else self.data.tail(count)
+
+        return __data__[timeframe]
 
     def get_all_indicator_names(self) -> list:
         return list(self.indicators.keys())
 
-    def __technicals__(self, time_frame) -> None:
-        return {
-            indicator: [
-                self.indicators[indicator][len(
-                    self.indicators[indicator]) - 1],
-                self.indicators[indicator][len(
-                    self.indicators[indicator]) - 2],
-            ]
-            for indicator in self.indicators
-            if "_"+time_frame+"_" in indicator
-        }
-    #6 updated trend
-    def Trend(self):
+    def __technicals__(self, time_frame: str, count=-1) -> Dict[str, pd.DataFrame]:
+        """Return a dictionary containing indicator as key (string) and its value as pandas DataFrame (using self.indicators DataFrame)
+
+        Args:
+            time_frame ([str]): time as string (e.g. "1m", "5m", "1h", "1d")
+            count (int, optional): number of rows to fetch for each indicator. Defaults to -1 (return all rows)
+
+        Returns:
+            Dict[str, pd.DataFrame]: key is indicator, value as pandas DataFrame
+        """
+        Indicators = {}
+        __indicators__ = self.indicators[time_frame] if count == -1 \
+            else self.indicators[time_frame].tail(count)
+
+        for indicator in __indicators__:
+            if "_" + time_frame + "_" in indicator:
+                Indicators[indicator] = __indicators__[indicator]
+
+        return Indicators
+
+    # 6 updated trend
+
+    def trend_find(self):
+        timeframe = self.config["primary_timeframe"]
+        informative_timeframe = self.config["informative_timeframe"]
+        # all parameters
+        # RSI_present,RSI_informative_all,close_informative_all,ema_short_present,close_present,
+        # ema_long_present,ema_short_previous,ema_long_previous,macd_hist_present,
+        # aroon_up_present,aroon_down_present,support_all,resistance_all,High(all)
+
+        candles = self.__get_candles__(timeframe,-1)
+        candles_informative = self.__get_candles__(informative_timeframe,-1)
+        values: dict = self.__technicals__(time_frame=timeframe)
+        informatives = self.__technicals__(informative_timeframe)
+        # if want latest value
+        self.marketpair = self.config["market_pair"][0]
+        ticker: float = self.__ticker_price__(force_now=False)
+        sr = self.sr
+        trends = {}
+        # generic
+        close_values_informatives=candles_informative["Close"].values
+        close_present=candles["Close"].values[0]
+        # RSI
+        rsi_lenght=str(self.config["periods"]["rsi"][1])
+        rsi_present=values["rsi"+"_"+timeframe+"_"+rsi_lenght][-1]
+        rsi_informative_lenght=self.config["limit"]["rsi"][0]
+        rsi_informative=informatives["rsi"+informative_timeframe+rsi_lenght]
+        RSI=self.rsi(rsi_present=rsi_present)
+        RSI_informative=self.rsi_Price_Slope(rsi_informative=rsi_informative
+        ,close_informative=close_values_informatives)
+        # EMA
+        ema_lenght=str(self.config["periods"]["ema"][1])
+        ema_short_present=values["ema"+"_"+timeframe+"_"+ema_lenght][-1]
+        ema_long_present=values["ema"+"_"+informative_timeframe+"_"+ema_lenght][-1]
+        ema_short_previous=values["ema"+"_"+timeframe+"_"+ema_lenght][-2]
+        ema_long_previous=values["ema"+"_"+informative_timeframe+"_"+ema_lenght][-2]
+        EMA_short=self.ema_Short(ema_short_present,close_present)
+        EMA_long=self.ema_Long(ema_long_present,close_present)
+        EMA_Short_Previous=self.ema_Short_Previous(ema_short_previous,ema_short_present)
+        EMA_Long_Previous=self.ema_Long_Previous(ema_long_previous,ema_long_present)
+        # MACD
+        Macd_lenght=str(self.config["periods"]["macd"][0])
+        Macd_presnt_histogram=values["macd"+"_"+timeframe+"_"+Macd_lenght][-1][2]
+        MACD=self.macd(Macd_presnt_histogram,close_present)
+        # Aroon up
+        aroon_up_lenght=str(self.config["periods"]["aroon"][0])
+        arron_up_present=values["aroon"+"_"+timeframe+"_"+aroon_up_lenght][-1]
+        AROON_UP=self.aroonUp(arron_up_present=arron_up_present)
+        # Aroon down
+        aroon_down_lenght=str(self.config["periods"]["aroon"][0])
+        arron_down_present=values["aroon"+"_"+timeframe+"_"+aroon_down_lenght][-1]
+        AROON_DOWN=self.aroonDown(aroon_down_present=aroon_down_lenght)
+        # Support and Resistance
+        SUPPORT_RESISTANCE=self.support_resistance(sr=sr,candles_all=candles,present_value=ticker)
+
+        # Trend
+        # for calculation of trend the upper bound and lower bound of multiplication of all parameters are used
+        # Based on the upper bound value and lower bound value trend value is calculated which is in between 0 and 1(with 4 decimal places)
+        # i.e theorotically if take upper bound as 1 and lower bound as 0 then trend value will be between 0 and 1
+
+        # weight distribution of each parameter is given i note.
+        TREND_MAX=None
+        TREND_MIN=None
+        TREND=RSI*EMA_short*EMA_long*EMA_Long_Previous*EMA_Short_Previous*MACD*AROON_UP*AROON_DOWN*SUPPORT_RESISTANCE
+        return TREND
+    def rsi(self,rsi_present):
+        # check if rsi value is in between 30 and 70
+        if rsi_present<30 or rsi_present>70:
+            return rsi_present
+    def rsi_Price_Slope(self,rsi_informative,close_informative):
+        # Implement the slope of the price and slope of rsi vals for 
+        # the last n candles
         pass
