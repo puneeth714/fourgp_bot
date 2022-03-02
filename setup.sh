913ba72ee5bea8fe476158a628562e6a15f173c6
pushyanth123 <kpushyanth136@gmail.com>
2022-03-02 20:48:28 +0530

    Created setup scripts for archlinux based distros

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 required_packages=(gcc g++ make python3.8 python3.8-pip python3.8-dev libblas-dev liblapack-dev pybind11 rust setuptools_rust openssl libssl-dev libjpeg  )
-pip_packages=(numpy scipy)
+pip_packages=(numpy matplotlib scikit-learn pandas scipy cython)
 
 # Function to check if installation succeeded
 post_operations()
@@ -17,34 +17,31 @@ post_operations()
     fi
 }
 
+# Function to install a packages
+InstallPackage() {
+    # TODO: Check distribution and install appropriate packages
+    linux_distro=$(cat /etc/os-release | grep "^ID=" | cut -d '=' -f 2)
+    if [[ $linux_distro == "ubuntu" ]]; then
+        sudo apt-get install $1
+    fi
+}
+
+
 # Install required packages
 echo "Installing required packages"
-sudo apt-get install ${required_packages[@]}
+InstallPackage ${required_packages[@]}
 post_operations "Required packages"
 
 
 # Create python3.8 virtual environment
 echo "Creating python3.8 virtual environment in python3.8-env folder"
-python3.8 -m venv python3.8-env
+python3 -m venv python3-env
 source python3.8-env/bin/activate
 echo "Virtual environment created.."
 
 # Install python libraries
 echo "Installing required python libraries..."
-for package in $pip_packages; do
-    echo "Installing $package"
-    pip3 install $package
-    post_operations $package
-done
-
-# install cython
-if [[ -z "$(pip show cython)" ]]; then  # If cython is not installed
-    echo "Installing cython"
-    pip install cython  # Install cython
-    post_operations "cython"    # Check if installation succeeded
-else
-    echo "cython already installed"
-fi
+pip3 install ${pip_packages[@]}
 
 
 # check if gcc is present
@@ -68,8 +65,7 @@ if [ -z "$(which ta-lib-config)" ]; then
     extract_dir="ta-lib"
     
     echo "Using $source_file as source file"
-    # Check if ta-lib-0.4.0-src.tar.gz is present in /tmp
-    if [ ! -f /tmp/$source_file ]; then
+    if [ ! -f /tmp/$source_file ]; then # CHeck if source file is already present
         echo "$source_file is not present in /tmp, downloading it"
         wget $source -O /tmp/$source_file   # Download source file
     else
@@ -78,24 +74,15 @@ if [ -z "$(which ta-lib-config)" ]; then
     
     echo "Extracting $source_file into $extract_dir"
     tar -xzf /tmp/$source_file  # Extract source file
-    cd $extract_dir # Change directory to ta-lib
+    cd $extract_dir 
     
-    # Install TA_Lib
-    ./configure
-    make    # build ta-lib
+    ./configure && make -j$(nproc)
     post_operations "TA_Lib"   
-    
-    
     sudo make install   # Install TA_Lib into system, need root access!!
     post_operations "TA_Lib system"
     
-    cd ..
-    rm -rf $extract_dir # Remove build directory
-
-    # Delete all variables
-    unset source
-    unset source_file
-    unset extract_dir
+    cd .. && rm -rf $extract_dir # Remove build directory
+    unset source source_file extract_dir     # Delete uneeded variables
 else
     echo "ta-lib is already installed. Continuing with installation"
 fi
